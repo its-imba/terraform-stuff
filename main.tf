b9fa3af7e9924de131812e8fced4f8e4762393d2
its-imba <its.sam.imba@gmail.com>
2023-05-17 11:07:42 +0100

    modify main.tf

--- main.tf
+++ main.tf
@@ -5,13 +5,18 @@ provider "aws" {
 resource "aws_instance" "demo1" {
   ami           = "ami-09744628bed84e434"
   instance_type = "t2.micro"
+
+  tags = {
+    Name         = "demo-instance"
+    S3_Bucket    = "sam-demo-bucket"
+  }
 }
 
 resource "aws_s3_bucket" "demo_bucket" {
   bucket = "sam-demo-bucket"
-}
 
-resource "aws_instance_s3_bucket_association" "demo_bucket_association" {
-  bucket      = aws_s3_bucket.demo_bucket.id
-  instance_id = aws_instance.demo1.id
+  tags = {
+    Name         = "sam-demo-bucket"
+    Associated_Instance = "demo-instance"
+  }
 }
